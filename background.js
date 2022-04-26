2340e171edd527636f77b25dd4f7ec329e2c7a6b
Akramsarker <dev.mdakram@gmail.com>
2022-04-27 04:17:58 +0600

    Updated some style

--- background.js
+++ background.js
@@ -9,28 +9,33 @@ chrome.webNavigation.onCompleted.addListener(({ tabId, frameId }) => {
 
 const newPageLoad = async () => {
   let darazInnerHTML = `<div>
-                          <h1 style="font-size: 2rem; margin-top: 1rem; margin-bottom: 1rem;">
+                          <h1 style="font-size: 2rem; margin: 0; padding: 10px 0;">
                             Similar Products
                           </h1>
-                          <div style="margin-top: 1rem; margin-bottom: 1rem;">
+                          <div style="">
                             <h3>Loading... </h3>
                           </div>
                         </div>`;
-  document.querySelector("#root").style.width = "1450px";
+  document.querySelector("#root").style.width = "1500px";
   document.querySelector(".pdp-block__product-detail").style.width =
-    "calc(100% - 615px)";
+    "calc(100% - 665px)";
   const createDev = document.createElement("div");
   createDev.className = "compare-product-info";
   createDev.innerHTML = darazInnerHTML;
   document.querySelector("#block-3FfF28kZyT").appendChild(createDev);
-  const styleDiv = document.querySelector(".compare-product-info");
-  styleDiv.style.display = "inline-block";
-  styleDiv.style.width = "262px";
-  styleDiv.style.backgroundColor = "#26ACD5";
-  styleDiv.style.padding = "10px";
-  styleDiv.style.color = "white";
-  styleDiv.style.height = "56vh";
-  styleDiv.style.overflowY = "scroll";
+  const styleDiv = document.querySelector(".compare-product-info").style;
+  styleDiv.display = "inline-block";
+  styleDiv.width = "315px";
+  styleDiv.backgroundColor = "#26ACD5";
+  styleDiv.padding = "0 10px";
+  styleDiv.color = "white";
+  styleDiv.height = "56vh";
+  styleDiv.overflowY = "auto";
+  // styleDiv:: -webkit-scrollbar-track {
+  //   -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  //   border-radius: 10px;
+  //   background-color: #F5F5F5;
+  // }
   if (location.href.includes("/products")) {
     const productName = document.querySelector(
       ".pdp-mod-product-badge-title"
@@ -44,33 +49,33 @@ const newPageLoad = async () => {
     if (success) {
       let similarProducts = "";
       posts.forEach((post) => {
-        similarProducts += `<a href="${
-          post.link
-        }" target="_blank" style="color: #fff; cursor: pointer; ">
-                              <div style="margin-bottom: 0.8rem; border: 1px solid #D0611E;">
-                                <div style="display: flex; padding: 5px;">
-                                   <img src="${
-                                     post.image
-                                   }" style="width: 70px;">
-                                   <div style="margin-left: 0.8rem;">
-                                     <p style="line-height: 1.5rem; font-size: 1.2rem;">${
-                                       post.title.slice(0, 35) + "…"
-                                     }</p>
-                                     <p style="padding-bottom: 0.7rem; padding-top: 1rem;">Price: ${
-                                       post.price
-                                     } TK</p>
-                                     <p>Shop Name: ${post.shop}</p>
-                                   </div>
+        similarProducts += `<div style="margin-bottom: 0.8rem; border: 1px solid #ffffff; borderRadius: 10px;">
+                               <div style="display: flex; padding: 5px;">
+                                  <img src="${
+                                    post.image
+                                  }" style="width: 70px; object-fit: cover;">
+                                <div style="margin-left: 0.8rem;"> 
+                                  <a   onMouseOver="this.style.color='#F6F1F0'" onMouseOut="this.style.color='#ffffff'"
+                                   href="${
+                                     post.link
+                                   }" target="_blank" style="line-height: 1.5rem; font-size: 1.2rem; color: #ffffff; cursor: pointer;  ">${
+          post.title.slice(0, 50) + "…"
+        }</a>
+                                  <p style="padding-bottom: 0.7rem; padding-top: 1rem;">${
+                                    post.price
+                                  } TK  &nbsp;&nbsp;${
+          post.shop.charAt(0).toUpperCase() + post.shop.slice(1)
+        }</p>
                                 </div>
-                              </div>
-                            </a>`;
+                            </div>
+                          </div>`;
       });
       console.log(similarProducts);
       darazInnerHTML = `<div>
-                            <h1 style="font-size: 2rem; margin-top: 1rem; margin-bottom: 1rem;">
+                            <h1 style="font-size: 2rem; margin: 0; padding: 10px 0;">
                              Similar Products
                             </h1>
-                            <div style="margin-top: 1rem; ">
+                            <div style=" ">
                               ${similarProducts}
                             </div>
                           </div>`;
